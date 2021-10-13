78187ea909bbf255f86cbb075de3fabeb2a61a0a
Kieranator5050 <thejugg63@gmail.com>
2021-10-13 16:19:15 -0500

    Sketched Layout

--- scripts.js
+++ scripts.js
@@ -1,1 +1,5 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerHTML = "Copyright 2020, Kieran Jaggernauth";
+});
