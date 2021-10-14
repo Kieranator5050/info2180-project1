976853971a57a26fffc22f8106337cb47dda1dd0
Kieranator5050 <thejugg63@gmail.com>
2021-10-13 19:08:37 -0500

    Testing Section Positioned

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 /* Add your JavaScript to this file */
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerHTML = "Copyright 2020, Kieran Jaggernauth";
+  document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerHTML = `<p>Copyright 2020, Kieran Jaggernauth</p><p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>`;
 });
