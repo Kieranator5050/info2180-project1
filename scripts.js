35407917ca9b4afe5e3d79d8dccbbeda1d4abc50
Kieranator5050 <thejugg63@gmail.com>
2021-10-13 23:19:58 -0500

    Testing Section Styled

--- scripts.js
+++ scripts.js
@@ -2,4 +2,5 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementsByTagName("footer")[0].getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerHTML = `<p>Copyright 2020, Kieran Jaggernauth</p><p>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>`;
+  document.getElementsByClassName("cta")[0].getElementsByTagName("a")[0].innerHTML = "LEARN MORE";
 });
